9f3f68ffd5676cecef392564b4e0be466958f942
kyohiro <kyohiro@gmail.com>
2014-08-25 16:39:47 +0000

    copy tmux config

--- make.sh
+++ make.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+is_mac=`uname | grep Darwin`
+cwd=`pwd`
 
+#install oh my zsh
 curl -L http://install.ohmyz.sh | sh
 chsh -s /bin/zsh
+
+#install tmux and copy config
+echo "copy tmux config"
+cp $cwd/tmux/* $HOME
+
+#vim config
